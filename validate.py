a3c8c8d9303800cded07c51cbc89348d668f1b11
Paulus <222410103052@unej.ac.id>
2024-12-01 22:34:48 +0700

    add regex and validate email-password in validate.py

--- validate.py
+++ validate.py
@@ -4,7 +4,7 @@ import re #Regex
 def validate_password(password):
     errors = []
     if not password:
-        errors.append("Password cannot be empty ")
+        errors.append("Password cannot be empty")
     elif len(password) < 8:
         errors.append("Password must 8 character or longer")
     return errors
@@ -14,7 +14,7 @@ def validate_email(email):
     errors = []
     email_regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]{2,}$'
     if not email:
-        errors.append("Email tidak boleh kosong")
+        errors.append("Email cannot be empty")
     elif not re.match(email_regex, email):
         errors.append("Email invalid")
     return errors
